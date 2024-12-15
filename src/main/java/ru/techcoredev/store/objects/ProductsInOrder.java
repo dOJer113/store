e32c3cfc2af279d0c17182c596ac6f0923fc8395
aleks <aleksandrroznov484@gmail.com>
2024-12-15 20:55:42 +0300

    small updates

--- src/main/java/ru/techcoredev/store/objects/ProductsInOrder.java
+++ src/main/java/ru/techcoredev/store/objects/ProductsInOrder.java
@@ -5,14 +5,21 @@ import jakarta.persistence.*;
 @Entity
 @Table(name = "products_in_order")
 public class ProductsInOrder {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
     @Column(name = "order_number")
     private int orderNumber;
+
+    @Column(name = "productid")
     private int productId;
+
     @Column(name = "count_product")
     private int countProduct;
-    public ProductsInOrder(){}
+
+    public ProductsInOrder() {}
 
     public ProductsInOrder(int orderNumber, int productId, int countProduct) {
         this.orderNumber = orderNumber;
@@ -20,16 +27,17 @@ public class ProductsInOrder {
         this.countProduct = countProduct;
     }
 
-    public int getCountProduct() {
-        return this.countProduct;
+
+    public int getId() {
+        return id;
     }
 
-    public void setCountProduct(int countProduct) {
-        this.countProduct = countProduct;
+    public void setId(int id) {
+        this.id = id;
     }
 
     public int getOrderNumber() {
-        return this.orderNumber;
+        return orderNumber;
     }
 
     public void setOrderNumber(int orderNumber) {
@@ -37,13 +45,20 @@ public class ProductsInOrder {
     }
 
     public int getProductId() {
-        return this.productId;
+        return productId;
     }
 
     public void setProductId(int productId) {
         this.productId = productId;
     }
 
+    public int getCountProduct() {
+        return countProduct;
+    }
+
+    public void setCountProduct(int countProduct) {
+        this.countProduct = countProduct;
+    }
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -51,6 +66,7 @@ public class ProductsInOrder {
 
         ProductsInOrder that = (ProductsInOrder) o;
 
+        if (getId() != that.getId()) return false;
         if (getOrderNumber() != that.getOrderNumber()) return false;
         if (getProductId() != that.getProductId()) return false;
         return getCountProduct() == that.getCountProduct();
@@ -58,7 +74,8 @@ public class ProductsInOrder {
 
     @Override
     public int hashCode() {
-        int result = getOrderNumber();
+        int result = getId();
+        result = 31 * result + getOrderNumber();
         result = 31 * result + getProductId();
         result = 31 * result + getCountProduct();
         return result;
@@ -67,7 +84,8 @@ public class ProductsInOrder {
     @Override
     public String toString() {
         return "ProductsInOrder{" +
-                "orderNumber=" + orderNumber +
+                "id=" + id +
+                ", orderNumber=" + orderNumber +
                 ", productId=" + productId +
                 ", countProduct=" + countProduct +
                 '}';
